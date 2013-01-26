c30f94a1f8868b3001a8bbb9221366d94f237b7f
Thierry Régagnon <thierry@regagnon.com>
2013-01-26 13:17:46 +0100

    More functional coding + now rates are cached in localStorage

--- js/app.js
+++ js/app.js
@@ -8,10 +8,9 @@
         decimalSep = ',';
 
 
-
     // Returns the exchange rate to `target` currency from `base` currency
     // Inspired from money.js
-    function getConversionRate(rates, from, to, base) {
+    var getConversionRate = function (rates, from, to, base) {
 
         // Make sure the base rate is in the rates object:
         rates[base] = 1;
@@ -34,63 +33,111 @@
         // Otherwise, return the `to` rate multipled by the inverse of the `from` rate to get the
         // relative exchange rate between the two currencies
         return rates[to] * (1 / rates[from]);
-    }
-
+    };
 
 
     // Clean a string from a user input and return a float
     // Remove space formatting and change , to . if the user use , as a decimal separator (check settings)
-    function prepareNumber(str) {
+    var prepareNumber = function (str) {
         str = str.replace(/ /g, '');
         if (decimalSep === ',') {
             str = str.replace(/,/g, '.');
         }
         return parseFloat(str);
-    }
+    };
 
 
+    var convertAmount = function (fromAmount, conversionRate) {
+        return fromAmount * conversionRate;
+    };
 
-    function getConversion(evt) {
-        evt.preventDefault();
+    var getRates = function () {
+        var key = 'rates',
+            data;
 
+        console.log('Get rates from localStorage');
+
+        data = localStorage.getItem(key);
+
+        return JSON.parse(data);
+    };
+
+    /********* Cache Data functions *********/
+
+    // Retrieve Data stored for later use (simple function for now, should be improved for no localStorage support)
+    var readData = function(key) {
+        return localStorage.getItem(key);
+    };
+    // Store Data for later retrieval (simple function for now, should be improved for no localStorage support)
+    var storeData = function(key, data) {
+        var date = Date();
+
+        localStorage.setItem(key, data);
+        localStorage.setItem(key + '-date', date.toString());
+    };
+
+
+    /********* Fetching Fresh Data functions *********/
+
+    var fetchRates = function(callback) {
         var xhr = new XMLHttpRequest(),
-            fromCurr = doc.getElementById('from-currency-select').value,
-            fromAmount = doc.getElementById('from-amount').value,
-            toCurr = doc.getElementById('to-currency-select').value,
-            toAmountEl = doc.getElementById('to-amount'),
-            response,
-            submitButton = doc.querySelector('input[type="submit"]');
-
-        fromAmount = prepareNumber(fromAmount);
-
-        console.log('Amount to convert: ' + fromAmount);
-
-        submitButton.setAttribute('disabled', 'disabled');
+            response;
 
         function transferComplete(evt) {
             var xhr = evt.target;
             if (xhr.status === 200 || xhr.status === 304) {
-              response = JSON.parse(xhr.responseText);
+                response = JSON.parse(xhr.responseText);
 
-              var conversionRate = getConversionRate(response.rates, fromCurr, toCurr, response.base);
+                console.log('Rates retrieved from Web Service');
 
-              console.log('Conversion Rate between ' + fromCurr + ' and ' + toCurr + ' = ' + conversionRate);
-
-              var result = fromAmount * conversionRate;
-              toAmountEl.value = result.toFixed(3);
+                if (typeof callback === 'function') {
+                    callback('rates', xhr.responseText);
+                }
 
             } else {
                 throw 'AJAX error';
             }
-
-            submitButton.removeAttribute('disabled');
         }
 
         xhr.addEventListener('load', transferComplete, false);
 
+        console.log('Start XHR for Rates');
         xhr.open('GET', url , true);
         xhr.send();
-    }
+    };
+
+    /********* View Related Functions *********/
+
+    var processForm = function (evt) {
+        evt.preventDefault();
+
+        var form = evt.target,
+            fromCurr = doc.getElementById('from-currency-select').value,
+            toCurr = doc.getElementById('to-currency-select').value,
+            fromAmount = doc.getElementById('from-amount').value,
+            submitButton = form.querySelector('input[type="submit"]'),
+            toAmountEl = doc.getElementById('to-amount'),
+            conversionRate,
+            rates,
+            result;
+
+        submitButton.setAttribute('disabled', 'disabled');
+
+        fromAmount = prepareNumber(fromAmount);
+        console.log('Amount to convert: ' + fromAmount);
+
+        rates = getRates();
+
+        conversionRate = getConversionRate(rates.rates, fromCurr, toCurr, rates.base);
+        console.log('Conversion Rate between ' + fromCurr + ' and ' + toCurr + ' = ' + conversionRate);
+
+        result = convertAmount(fromAmount, conversionRate);
+
+        // Display converted amount
+        toAmountEl.value = result.toFixed(3);
+
+        submitButton.removeAttribute('disabled');
+    };
 
     // Get up to date list of currencies from openexchanges rates and create currencies dropdown in the form
     function setupCurrenciesSelect(containers) {
@@ -113,11 +160,11 @@
 
             for ( currency in currencies ) {
 
-                    optionEl = doc.createElement('option');
-                    optionTxt = doc.createTextNode(currency + ' - ' + currencies[currency]);
-                    optionEl.value = currency;
-                    optionEl.appendChild(optionTxt);
-                    currenciesList.appendChild(optionEl);
+                optionEl = doc.createElement('option');
+                optionTxt = doc.createTextNode(currency + ' - ' + currencies[currency]);
+                optionEl.value = currency;
+                optionEl.appendChild(optionTxt);
+                currenciesList.appendChild(optionEl);
             }
 
             var currenciesList2 = currenciesList.cloneNode(true);
@@ -136,12 +183,32 @@
         }
     }
 
-setupCurrenciesSelect(
-    [
+    setupCurrenciesSelect([
         doc.getElementById('from-currency'),
         doc.getElementById('to-currency')
     ]);
 
-form.addEventListener('submit', getConversion, false);
+    var init = function() {
+
+        console.log('Start App init');
+
+        // set Rates
+        var ratesDate = readData('rates-date'),
+            now = Date.now(),
+            diffDate,
+            cacheLimit = 86400000;
+
+        ratesDate = Date.parse(ratesDate);
+        diffDate =  now - ratesDate.value;
+
+        if (diffDate > cacheLimit) {
+            console.log('Cached rates too old, retrieving fresh rates');
+            fetchRates(storeData);
+        }
+    };
+
+    doc.addEventListener('DOMContentLoaded', init, false);
+
+    form.addEventListener('submit', processForm, false);
 
 })(document);
